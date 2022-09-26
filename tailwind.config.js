43732479f1e32aed79d9868079fd4e6464ce81fb
CongSanhLe <congsanhle1999@gmail.com>
2022-09-27 02:35:04 +0700

    [27092022] - ecommerce first setup

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*html"],
+  theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "922px",
+      xl: "1200px",
+    },
+
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+
+    extend: {
+      colors: {
+        primary: "#FD3D57",
+      },
+
+      fontFamily: {
+        poppins: ["Poppins"],
+        roboto: ["Roboto"],
+      },
+    },
+  },
+  plugins: [],
+};
